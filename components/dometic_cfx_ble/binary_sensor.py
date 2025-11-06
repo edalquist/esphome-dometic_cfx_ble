178c176537975187b576c3458e0a4580f9b323f6
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2025-11-06 19:57:20 +1100

    a

--- components/dometic_cfx_ble/binary_sensor.py
+++ components/dometic_cfx_ble/binary_sensor.py
@@ -2,7 +2,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_TYPE, CONF_NAME
-from esphome.cpp_types import binary_sensor as esphome_binary_sensor
+from esphome.components import binary_sensor as esphome_binary_sensor
 
 from . import dometic_cfx_ble_ns, DometicCfxBle, CONF_DOMETIC_CFX_BLE_ID, validate_topic_type
 
